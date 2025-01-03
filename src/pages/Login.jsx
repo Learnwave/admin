0c2732c780de83bf3b7f8016542d22f2f240f4a6
Learnwave <146640293+Learnwave@users.noreply.github.com>
2025-01-03 22:00:36 +0530

    new

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import {assets} from '../assets/assets_admin/assets'
+const Login = () => {
+
+    const [state,setState] = useState('Admin')
+    
+
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
